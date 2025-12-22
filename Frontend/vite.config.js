26cf5944b6f2f834adfe061a37e737b2bb9bbcc8
KarimMaoui <karim.maoui@edu.em-lyon.com>
2025-12-22 21:10:05 +0100

    Update vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: process.env.VITE_BASE_PATH || "/Upward",
-});
+  base: command === "build" ? "/Upward/" : "/",
+}));
