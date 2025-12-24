cecac024b935604d7d8221da59c607fea5165ead
KarimMaoui <karim.maoui@edu.em-lyon.com>
2025-12-25 00:04:54 +0100

    Update vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig(({ command }) => ({
+export default defineConfig({
   plugins: [react()],
-  base: command === "build" ? "/Upward/" : "/",
-}));
+  base: "/", // On force la racine, c'est tout ce qu'il faut pour Vercel
+});
